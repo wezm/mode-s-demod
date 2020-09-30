4d7ec6254f7bce2ac3337fd223bcdeb1182b3a8e
Wesley Moore <wes@wezm.net>
2020-09-30 20:18:04 +1000

    Move modesMessageLenByType to mode_s module

--- lib.rs
+++ lib.rs
@@ -1,19 +1,4 @@
-use std::os::raw::c_int;
-
-const MODES_LONG_MSG_BYTES: c_int = 14;
-const MODES_SHORT_MSG_BYTES: c_int = 7;
-const MODES_LONG_MSG_BITS: c_int = MODES_LONG_MSG_BYTES * 8;
-const MODES_SHORT_MSG_BITS: c_int = MODES_SHORT_MSG_BYTES * 8;
-
-#[no_mangle]
-pub extern "C" fn modesMessageLenByType(type_: c_int) -> c_int {
-    // (type_ & 0x10) ? MODES_LONG_MSG_BITS : MODES_SHORT_MSG_BITS ;
-    if type_ & 0x10 == 0x10 {
-        MODES_LONG_MSG_BITS
-    } else {
-        MODES_SHORT_MSG_BITS
-    }
-}
+mod mode_s;
 
 #[cfg(test)]
 mod tests {
