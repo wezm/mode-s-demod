0bb63318837471d9dd8f469fb3bc91833e02c95b
Wesley Moore <wes@wezm.net>
2020-10-06 20:57:47 +1100

    Port cprNLFunction to Rust

--- mode_s.rs
+++ mode_s.rs
@@ -4,7 +4,7 @@ use std::cmp::Ordering;
 use std::convert::TryFrom;
 use std::ffi::CStr;
 use std::io::Write;
-use std::os::raw::{c_char, c_int, c_uchar, c_uint, c_ulong, c_void};
+use std::os::raw::{c_char, c_double, c_int, c_uchar, c_uint, c_ulong, c_void};
 use std::time::SystemTime;
 use std::{io, mem, ptr, time};
 
@@ -2206,6 +2206,192 @@ pub extern "C" fn cprModFunction(a: c_int, b: c_int) -> c_int {
     } else {
         res
     }
+}
+
+// The NL function uses the precomputed table from 1090-WP-9-14
+//
+#[no_mangle]
+pub extern "C" fn cprNLFunction(mut lat: c_double) -> c_int {
+    if lat < 0.0 {
+        // Table is symmetric about the equator
+        lat = -lat
+    }
+    if lat < 10.47047130f64 {
+        return 59;
+    }
+    if lat < 14.82817437f64 {
+        return 58;
+    }
+    if lat < 18.18626357f64 {
+        return 57;
+    }
+    if lat < 21.02939493f64 {
+        return 56;
+    }
+    if lat < 23.54504487f64 {
+        return 55;
+    }
+    if lat < 25.82924707f64 {
+        return 54;
+    }
+    if lat < 27.93898710f64 {
+        return 53;
+    }
+    if lat < 29.91135686f64 {
+        return 52;
+    }
+    if lat < 31.77209708f64 {
+        return 51;
+    }
+    if lat < 33.53993436f64 {
+        return 50;
+    }
+    if lat < 35.22899598f64 {
+        return 49;
+    }
+    if lat < 36.85025108f64 {
+        return 48;
+    }
+    if lat < 38.41241892f64 {
+        return 47;
+    }
+    if lat < 39.92256684f64 {
+        return 46;
+    }
+    if lat < 41.38651832f64 {
+        return 45;
+    }
+    if lat < 42.80914012f64 {
+        return 44;
+    }
+    if lat < 44.19454951f64 {
+        return 43;
+    }
+    if lat < 45.54626723f64 {
+        return 42;
+    }
+    if lat < 46.86733252f64 {
+        return 41;
+    }
+    if lat < 48.16039128f64 {
+        return 40;
+    }
+    if lat < 49.42776439f64 {
+        return 39;
+    }
+    if lat < 50.67150166f64 {
+        return 38;
+    }
+    if lat < 51.89342469f64 {
+        return 37;
+    }
+    if lat < 53.09516153f64 {
+        return 36;
+    }
+    if lat < 54.27817472f64 {
+        return 35;
+    }
+    if lat < 55.44378444f64 {
+        return 34;
+    }
+    if lat < 56.59318756f64 {
+        return 33;
+    }
+    if lat < 57.72747354f64 {
+        return 32;
+    }
+    if lat < 58.84763776f64 {
+        return 31;
+    }
+    if lat < 59.95459277f64 {
+        return 30;
+    }
+    if lat < 61.04917774f64 {
+        return 29;
+    }
+    if lat < 62.13216659f64 {
+        return 28;
+    }
+    if lat < 63.20427479f64 {
+        return 27;
+    }
+    if lat < 64.26616523f64 {
+        return 26;
+    }
+    if lat < 65.31845310f64 {
+        return 25;
+    }
+    if lat < 66.36171008f64 {
+        return 24;
+    }
+    if lat < 67.39646774f64 {
+        return 23;
+    }
+    if lat < 68.42322022f64 {
+        return 22;
+    }
+    if lat < 69.44242631f64 {
+        return 21;
+    }
+    if lat < 70.45451075f64 {
+        return 20;
+    }
+    if lat < 71.45986473f64 {
+        return 19;
+    }
+    if lat < 72.45884545f64 {
+        return 18;
+    }
+    if lat < 73.45177442f64 {
+        return 17;
+    }
+    if lat < 74.43893416f64 {
+        return 16;
+    }
+    if lat < 75.42056257f64 {
+        return 15;
+    }
+    if lat < 76.39684391f64 {
+        return 14;
+    }
+    if lat < 77.36789461f64 {
+        return 13;
+    }
+    if lat < 78.33374083f64 {
+        return 12;
+    }
+    if lat < 79.29428225f64 {
+        return 11;
+    }
+    if lat < 80.24923213f64 {
+        return 10;
+    }
+    if lat < 81.19801349f64 {
+        return 9;
+    }
+    if lat < 82.13956981f64 {
+        return 8;
+    }
+    if lat < 83.07199445f64 {
+        return 7;
+    }
+    if lat < 83.99173563f64 {
+        return 6;
+    }
+    if lat < 84.89166191f64 {
+        return 5;
+    }
+    if lat < 85.75541621f64 {
+        return 4;
+    }
+    if lat < 86.53536998f64 {
+        return 3;
+    }
+    if lat < 87.00000000f64 {
+        return 2;
+    } else {
+        return 1;
+    };
 }
 
 #[cfg(test)]
